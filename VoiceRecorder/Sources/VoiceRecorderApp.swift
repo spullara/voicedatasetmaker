f0d37cce0c93296f41c63aacd6bc18879c29c64b
Sam Pullara <spullara@gmail.com>
2026-01-28 12:51:27 -0800

    update gitignore

--- VoiceRecorder/Sources/VoiceRecorderApp.swift
+++ VoiceRecorder/Sources/VoiceRecorderApp.swift
@@ -1,9 +1,18 @@
 import SwiftUI
+import AppKit
 
 @main
 struct VoiceRecorderApp: App {
     @StateObject private var appState = AppState()
-    
+
+    init() {
+        // Force the app to the foreground - required for SwiftUI apps run via SPM
+        DispatchQueue.main.async {
+            NSApp.setActivationPolicy(.regular)
+            NSApp.activate(ignoringOtherApps: true)
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
